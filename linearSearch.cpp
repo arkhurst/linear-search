3b2082d191a29e26384866c993b3595d72ffdba0
unknown <fiiarkhurst@gmail.com>
2020-02-23 10:34:47 +0000

    First commit

---
+++ linearSearch.cpp
@@ -0,0 +1,23 @@
+#include<studio.h>
+using namespace std;
+
+int LinearSearch (int [], int, int);
+
+int main (void){
+	
+	int arr[20],  num, key;
+	int i, iPosition = 0;
+	printf("\nEnter the size of the array\n");
+	scanf("%d",&num);
+	printf("\nEnter the key element\n");
+	scanf('%d',&key);
+	
+	iPosition = LinearSearch (arr, key, num);
+	
+	if (iPostion==-1)
+	   printf("\n Element not found \n");
+	else
+	   printf("\n Element found at postion %d\n", iPosition ) ;
+	   
+	return 0; 
+}
